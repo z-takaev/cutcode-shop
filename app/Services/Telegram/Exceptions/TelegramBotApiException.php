c91d5afaeb847338c435e28a765f63df9214bbeb
z-takaev <zatakata.web@gmail.com>
2022-10-21 16:10:07 +0300

    Create TelegramBotApiException

---
+++ app/Services/Telegram/Exceptions/TelegramBotApiException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Services\Telegram\Exceptions;
+
+class TelegramBotApiException extends \Exception
+{
+}
--- app/Services/Telegram/TelegramBotApi.php
+++ app/Services/Telegram/TelegramBotApi.php
@@ -2,19 +2,26 @@
 
 namespace App\Services\Telegram;
 
+use App\Services\Telegram\Exceptions\TelegramBotApiException;
 use Illuminate\Support\Facades\Http;
 
 class TelegramBotApi
 {
-    public const HOST = 'https://api.telegram.org/bot';
+    public const HOST = 'https://api.telegram.org/bo';
 
     static function sendMessage(string $token, int $chatId, string $text): bool
     {
-        $response = Http::acceptJson()->get(self::HOST . $token . '/sendMessage', [
-            'chat_id' => $chatId,
-            'text' => $text,
-        ])->json();
+        try {
+            $response = Http::acceptJson()->get(self::HOST . $token . '/sendMessage', [
+                'chat_id' => $chatId,
+                'text' => $text,
+            ])->throw()->json();
 
-        return $response['ok'];
+            return $response['ok'] ?? false;
+        } catch (\Throwable $e) {
+            report(new TelegramBotApiException($e->getMessage()));
+
+            return false;
+        }
     }
 }
