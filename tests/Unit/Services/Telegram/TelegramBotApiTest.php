55b0a4625b3433fe83b4a3be85ec9e53b29f9e75
z-takaev <zatakata.web@gmail.com>
2022-11-03 19:17:15 +0300

    Create telegram unit test

---
+++ tests/Unit/Services/Telegram/TelegramBotApiTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Unit\Services\Telegram;
+
+use Illuminate\Support\Facades\Http;
+use Services\Telegram\TelegramBotApi;
+use Tests\TestCase;
+
+class TelegramBotApiTest extends TestCase
+{
+    /**
+     * @test
+     * @return void
+     */
+    public function it_send_message_success(): void
+    {
+        Http::fake([
+            TelegramBotApi::HOST . '*' => Http::response(['ok' => true])
+        ]);
+
+        $response = TelegramBotApi::sendMessage('', 1, 'Testing');
+
+        $this->assertTrue($response);
+    }
+}
