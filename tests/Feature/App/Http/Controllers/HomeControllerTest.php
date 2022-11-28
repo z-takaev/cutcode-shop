798f9ed2a550ebf3d71a934099f96a8e78a4ffa4
z-takaev <zatakata.web@gmail.com>
2022-11-28 16:16:05 +0300

    Add test for Homepage

--- tests/Feature/App/Http/Controllers/HomeControllerTest.php
+++ tests/Feature/App/Http/Controllers/HomeControllerTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Feature\App\Http\Controllers;
 
+use App\Models\Product;
+use Database\Factories\BrandFactory;
+use Database\Factories\CategoryFactory;
+use Database\Factories\ProductFactory;
 use Tests\TestCase;
 
 class HomeControllerTest extends TestCase
@@ -12,11 +16,44 @@ class HomeControllerTest extends TestCase
      */
     public function it_home_page_success(): void
     {
+        BrandFactory::new()
+            ->count(5)
+            ->create([
+                'on_home_page' => true,
+                'sorting' => 999
+            ]);
+
+        $brand = BrandFactory::new()
+            ->createOne([
+                'on_home_page' => true,
+                'sorting' => 1,
+            ]);
+
+        ProductFactory::new()
+            ->count(5)
+            ->create([
+                'on_home_page' => true,
+                'sorting' => 999
+            ]);
+
+        $product = ProductFactory::new()
+            ->createOne([
+                'on_home_page' => true,
+                'sorting' => 1,
+            ]);
+
+        $categories = CategoryFactory::new()
+            ->count(5)
+            ->create();
+
         $response = $this->get(route('home'));
 
         $response
             ->assertOk()
             ->assertViewIs('home')
-            ->assertSee('Главная страница');
+            ->assertSee('Главная страница')
+            ->assertViewHas('brands.0', $brand)
+            ->assertViewHas('products.0', $product)
+            ->assertViewHas('categories', $categories);
     }
 }
